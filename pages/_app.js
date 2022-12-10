1ae4f26747662e9930bdfac3bf3732e407d3a594
georgetegral <jorgerenemg@icloud.com>
2022-12-10 22:10:44 +0100

    Added AppLayout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import AppLayout from "../src/layout/AppLayout";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AppLayout>
+        <Head>
+          <title>Notifications</title>
+        </Head>
+        <Component {...pageProps} />
+      </AppLayout>
     </ChakraProvider>
   )
 }
---
+++ src/layout/AppLayout.js
@@ -0,0 +1,48 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { BellIcon } from '@chakra-ui/icons';
+
+export default function AppLayout({children}) {
+  return (
+    <>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+            <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                <Box>Resilia</Box>
+                <Flex alignItems={'center'}>
+                    <Menu>
+                    <MenuButton
+                        size={'md'}
+                        rounded={'full'}
+                        minW={5}>
+                        <BellIcon />
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>Link 1</MenuItem>
+                        <MenuItem>Link 2</MenuItem>
+                        <MenuDivider />
+                        <MenuItem>Link 3</MenuItem>
+                    </MenuList>
+                    </Menu>
+                </Flex>
+            </Flex>
+        </Box>
+
+        {children}
+    </>
+  );
+}
